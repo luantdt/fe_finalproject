5898b9044984f6b76433b6cdb71a3770e4e6c8d4
Tran Vu Luan <vuluan5091@gmail.com>
2021-03-31 23:48:25 +0700

    change

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import TrangChonDoTuoi from './pages/TrangChonDoTuoi';
 import TrangChu from './pages/TrangChu';
 import Footer from './widget/Footer';
 import Header from './widget/Header';
+import ButtonContact from './widget/ButtonContact'
 import TrangSanPham from './pages/TrangSanPham';
 import {
   BrowserRouter as Router,
@@ -11,6 +12,7 @@ import {
   Redirect,
 } from 'react-router-dom';
 import { useEffect, useState } from 'react';
+
 function App() {
   const [checkAge,setCheckAge] = useState(false);
   useEffect(() => {
@@ -22,6 +24,7 @@ function App() {
   return (
     <>
       <Router>
+        <ButtonContact />
         {(checkAge)? '' : <Header />}
         {(checkAge)? <Redirect to="/choose-age"/>: ''}
         <Switch>
